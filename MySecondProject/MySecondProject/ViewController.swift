89882ce6f70c54537fd802ea4566178bd37cd337
Mirae Kim <77021071+mirae0312@users.noreply.github.com>
2023-11-24 22:33:09 +0900

    Add files via upload

---
+++ MySecondProject/MySecondProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySecondProject
+//
+//  Created by mirae on 11/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
